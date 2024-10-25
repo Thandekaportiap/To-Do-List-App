c0999a1a346ccadb4050d9501feb69485f4af442
Thandekaportiap <thandekaportiap05@gmail.com>
2024-10-25 09:18:42 +0200

    feat: packeges

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000, 
-    strictPort: true,
-  },
-});
+})
+
